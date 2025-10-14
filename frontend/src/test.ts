3cc9571c518173a49d875c963fc09f14db0222b3
Sveva Mazzari <smazzari88@gmail.com>
2025-10-14 11:35:45 +0200

    create class Paddle

--- frontend/src/test.ts
+++ frontend/src/test.ts
@@ -1,5 +1,7 @@
 //HTML elements
 const canva : any = document.getElementById("gameCanva");
+const ctx = canva.getContext('2d');
+
 const gameP : any = document.getElementById('gamePage');
 const chatP : any = document.getElementById('chatPage');
 const leadP : any = document.getElementById('leaderboardPage');
@@ -11,15 +13,14 @@ const matchTypeButton : any = document.getElementById('matchType');
 function display(text: string) : void
 {
 	console.log(`${text}`);
-	if (text == "game")
-	{
-		chatP.style.display = "none";
-		leadP.style.display = "none";
-		welcP.style.display = "none";
-		gameP.style.display = "flex";
-
-	}
-	else if (text == "chat")
+	// if (text == "game")
+	// {
+	// 	chatP.style.display = "none";
+	// 	leadP.style.display = "none";
+	// 	welcP.style.display = "none";
+	// 	gameP.style.display = "flex";
+	// }
+	if (text == "chat")
 	{
 		gameP.style.display = "none";
 		leadP.style.display = "none";
@@ -45,27 +46,67 @@ function setSettingPlayerType(button : string)
 function setSettingPlayerMode(button : string)
 {
 	console.log(`${button}`);
-	playerTypeButton.style.display = "none";
+	playerModeButton.style.display = "none";
 	matchTypeButton.style.display = "flex";
 }
 
 function setSettingMatchType(button : string)
 {
 	console.log(`${button}`);
+	matchTypeButton.style.display = "none";
+	canva.style.display = "block";
+	startGame();
 }
 
 //Game
+class Paddle {
+	width: number;
+	height: number;
+	color: string;
+	v: number; //only fixed property?
+	x : number;
+	y : number;
 
-class Paddles {
+	constructor() {
+	this.height = canva.height * 0.15;
+	this.width = this.height / 4;
+	this.color = "white";
+	this.v = 7;
+	this.x = 0;
+	this.y = 0;
+	};
 
+	positionPaddle(x : number, y : number) {
+		this.x = x;
+		this.y = y;
+	}
+
+	drawPaddle () {
+		ctx.beginPath();
+		ctx.rect(this.x, this.y, this.width, this.height);
+		ctx.fillStyle = this.color;
+		ctx.fill();
+		ctx.closePath();
+	}
 }
-// class Ball {
 
-// }
-// class Game {
-// 	constructor() {};
-// 	//instances
-// 	playerType
-// 	playerMode
-// 	matchType
-// }
+function startGame() : void {
+
+	chatP.style.display = "none";
+	leadP.style.display = "none";
+	welcP.style.display = "none";
+	gameP.style.display = "flex";
+
+	// let dpi = window.devicePixelRatio;
+	// canva.width = canva.width*dpi;
+    // canva.height = canva.height*dpi;
+    // ctx.scale(dpi, dpi);
+	ctx.beginPath();
+	let leftPaddle : Paddle = new Paddle();
+	let rightPaddle : Paddle = new Paddle();
+	ctx.clearRect(0, 0, canva.width, canva.height);
+	leftPaddle.positionPaddle(0, canva.height / 2 - leftPaddle.height / 2);
+	rightPaddle.positionPaddle(canva.width - rightPaddle.width, canva.height / 2 - leftPaddle.height / 2);
+	leftPaddle.drawPaddle();
+	rightPaddle.drawPaddle();
+}
