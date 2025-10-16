e2620b20099675ef2e89cb744b97f8efbbffa696
Sveva Mazzari <smazzari88@gmail.com>
2025-10-16 18:06:24 +0200

    create class to retrive chat info and create functino to add div elements in html to display

--- frontend/src/chat_page.ts
+++ frontend/src/chat_page.ts
@@ -1,6 +1,33 @@
-const chatElement : any = document.getElementById('chatPage')
+const currentDiv : any = document.getElementById("list-dms");
 
-function displayChatPage(text: string) : void
+class chatPrevInfo {
+	id : number;
+	recipientName : string;
+	avatar : string;
+
+	constructor (id : number, rec : string, avatar : string) {
+		this.id = id;
+		this.recipientName = rec;
+		this.avatar = avatar;
+	}
+}
+
+function addElement(name : string) {
+  
+	const newDiv = document.createElement("div");
+    const newContent = document.createTextNode(name);
+	newDiv.appendChild(newContent);
+    newDiv.className = "chat-left__list-dms-item border";
+    currentDiv.append(newDiv);
+}
+
+let chatPrevList : chatPrevInfo[] = [];
+for (let step = 0; step < 24; step++)
 {
-	chatElement.style.display = "block";
+	let chat : chatPrevInfo = new chatPrevInfo(1, "chat " + step, "url");
+	chatPrevList.push(chat);
+}
+
+for (let step = 0; step < chatPrevList.length; step++) {
+	addElement(chatPrevList[step].recipientName);
 }
