9361a70ba71621f042e8622738123e8a076cc565
Sveva Mazzari <smazzari88@gmail.com>
2025-10-17 09:00:21 +0200

    create event on preview box to match the coversation

--- frontend/src/chat_page.ts
+++ frontend/src/chat_page.ts
@@ -1,33 +1,85 @@
-const currentDiv : any = document.getElementById("list-dms");
+const listDmsDiv : any = document.getElementById("list-dms");
+const bubbleDiv : any = document.getElementById("bubble");
 
-class chatPrevInfo {
-	id : number;
+class Chat {
+	id : string;
 	recipientName : string;
 	avatar : string;
+	messages : {sender : boolean; receiver: boolean; content : string}[]
 
-	constructor (id : number, rec : string, avatar : string) {
+	constructor (id : string, rec : string, avatar : string, messages : {sender : boolean; receiver: boolean; content : string}[]) {
 		this.id = id;
 		this.recipientName = rec;
 		this.avatar = avatar;
+		this.messages = messages;
 	}
 }
 
-function addElement(name : string) {
+function addElement(name : string, id : string) {
   
 	const newDiv = document.createElement("div");
     const newContent = document.createTextNode(name);
+	newDiv.setAttribute('id', id);
+	newDiv.onclick = function() { dispalyConversationHistory(newDiv.id, chatList)};
 	newDiv.appendChild(newContent);
     newDiv.className = "chat-left__list-dms-item border";
-    currentDiv.append(newDiv);
+    listDmsDiv.append(newDiv);
 }
 
-let chatPrevList : chatPrevInfo[] = [];
-for (let step = 0; step < 24; step++)
+function addBubble(role : string, content : string)
 {
-	let chat : chatPrevInfo = new chatPrevInfo(1, "chat " + step, "url");
-	chatPrevList.push(chat);
+	const newBubble = document.createElement("div");
+	const newContent = document.createTextNode(content);
+	newBubble.append(newContent);
+	if (role == "sender")
+	{
+		newBubble.className = "chat-right__bubble-sent border";
+	}
+	else
+	{
+		newBubble.className = "chat-right__bubble-received border";
+	}
+	bubbleDiv.append(newBubble);
 }
 
-for (let step = 0; step < chatPrevList.length; step++) {
-	addElement(chatPrevList[step].recipientName);
+function dispalyConversationHistory(id : string, list : Chat[]) {
+	for (let step = 0; step < list.length; step++)
+	{
+		if (list[step].id == id)
+		{
+			console.log(`id: ${list[step].id}`);
+			let historyMex : { sender: boolean; receiver: boolean; content: string }[] = list[step].messages;
+			for (let step = 0; step < historyMex.length; step++)
+			{
+				if (historyMex[step].sender == true)
+					addBubble("sender", historyMex[step].content);
+				else
+					addBubble("recv", historyMex[step].content);
+			}
+		}
+	}
 }
+
+let chatList : Chat[] = [];
+let messages1: { sender: boolean; receiver: boolean; content: string }[] = [];
+let message1 : { sender: boolean; receiver: boolean; content: string } = {sender : true, receiver : false , content : 'Hello there'};
+let message2 : { sender: boolean; receiver: boolean; content: string } = {sender : false, receiver : true , content : 'hi'};
+let message3 : { sender: boolean; receiver: boolean; content: string } = {sender : false, receiver : true , content : 'how are you?'};
+messages1.push(message1);
+messages1.push(message2);
+messages1.push(message3);
+let messages2:  { sender: boolean; receiver: boolean; content: string }[] = [];
+let message4 : { sender: boolean; receiver: boolean; content: string } = {sender : false, receiver : true , content : 'hallo'};
+let message5 : { sender: boolean; receiver: boolean; content: string } = {sender : true, receiver : false , content : 'guten abend'};
+let message6 : { sender: boolean; receiver: boolean; content: string } = {sender : true, receiver : false , content : 'tschussi'};
+messages2.push(message4);
+messages2.push(message5);
+messages2.push(message6);
+chatList.push(new Chat('1', "chat " + 1, "url", messages1));
+chatList.push(new Chat('2', "chat " + 2, "url", messages2));
+
+for (let step = 0; step < chatList.length; step++) {
+	addElement(chatList[step].recipientName, chatList[step].id);
+}
+
+
