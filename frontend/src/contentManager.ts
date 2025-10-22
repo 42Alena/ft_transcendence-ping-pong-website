804eb86d5aa586e2bd8de75936e73894c86bf8e4
Sveva Mazzari <smazzari88@gmail.com>
2025-10-22 18:43:45 +0200

    added login page layout

--- frontend/src/contentManager.ts
+++ frontend/src/contentManager.ts
@@ -1,6 +1,6 @@
 const chatP : any = document.getElementById('chatPage');
 const welcP : any = document.getElementById('welcomePage');
-const profP : any = document.getElementById('account');
+const accP : any = document.getElementById('account');
 
 function displayPage(text: string) : void
 {
@@ -8,17 +8,17 @@ function displayPage(text: string) : void
 	{
 		chatP.style.display = "grid";
 		welcP.style.display = "none";
-		profP.style.display = "none";
+		accP.style.display = "none";
 	}
 	else if(text == 'welcome')
 	{
 		welcP.style.display = "flex";
 		chatP.style.display = "none";
-		profP.style.display = "none";
+		accP.style.display = "none";
 	}
 	else if (text == 'profile')
 	{
-		profP.style.display = "flex";
+		accP.style.display = "flex";
 		chatP.style.display = "none";
 		welcP.style.display = "none";
 	}
