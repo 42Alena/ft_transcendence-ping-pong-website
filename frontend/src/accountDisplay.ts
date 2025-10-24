fa185027689012b0d335acbe6d05ad7864ab25dc
svevotti <111067594+svevotti@users.noreply.github.com>
2025-10-24 06:39:07 +0200

    Sveva/login registration layout (#83)
    Created three new pages
    
    1. Display choice between registration and login (2 buttons)
    2. Registration form : 4 fields - avatar, nickname, name, password
    (added checkbox of gdpr for not forgetting about it of me)
    3. Login form: 2 fields - name and password.
    
    ~~I have been researching for 2 days how to test it, but without a
    server I wasn't capable to find a solution. I think we can discuss how
    to move forward.~~ Problem fixed by cleaning my caches..
    
    I believe what you care about is the form itself:
    from my understanding, you should reference to the forms in ts by their
    id, 'register' for registration form and 'login' for login form.
    the buttons in both forms are "submit" type, and it made it very clear
    to me that I can't do much without some response from a server.
    ~~I also tried with 'button' type, but without submission, password
    field breaks my account page logic.~~ Problem fixed by cleaning cache
    and add an event on the form and not on the button.
    
    I was able to retrieve the data from the forms by creating a FormData
    (obj type returned by form in key, value pair where key is "name"
    attribute in the input element), but I failed to display it on the
    screen (only on console). ->update: I can display on the screen
    
    What I am missing:
    - message password validation (we can set up rules) - now all fields are
    set to be required, so can't submit without filling in the blanks
    - validation password frontend (adding attributes in html like length,
    special characters etc etc)
    - correct flow between these three pages (see below) - without
    submission it works (problem with persistent data tho)
    - implement logic to display choice page if user isn't logged in, and
    display profile if user is logged in - solved
    - display avatar chosen by user (also need to have default option(s))
    
    the missing parts will be more clear after form submission and response
    from server

---
+++ frontend/src/accountDisplay.ts
@@ -0,0 +1,104 @@
+const registerPage : any = document.getElementById("registerPage");
+const loginPage : any = document.getElementById("loginPage");
+const profilePage : any = document.getElementById("profilePage");
+const choicePage : any = document.getElementById("pageChoice");
+
+const submitButton : any = document.getElementById("submitRegButton");
+const loginButton : any = document.getElementById("submitLogingButton");
+
+const registerForm : any = document.getElementById("register");
+const loginForm : any = document.getElementById("login");
+
+const regAvatar : any = document.getElementById("reg-avatar");
+
+const profName : any = document.getElementById("acc-name");
+const profNickname : any = document.getElementById("acc-nickname");
+const profAvatar : any = document.getElementById("acc-avatar");
+
+let userIsLoggedIn : boolean = false;
+
+function setAccountPage(text : string)
+{
+	if (text == "login")
+	{
+		loginPage.style.display = "flex";
+		choicePage.style.display = "none";
+		registerPage.style.display = "none";
+		profilePage.style.display = "none";
+
+	}
+	else if (text == "register")
+	{
+		registerPage.style.display = "flex";
+		choicePage.style.display = "none";
+		loginPage.style.display = "none";
+		profilePage.style.display = "none";
+	}
+}
+
+regAvatar.addEventListener("change", (event : any) => {
+	console.log('change event caught');
+	const img : any = document.getElementById("avatar");
+	img.src = URL.createObjectURL(regAvatar.files[0]);
+});
+
+// /*Just to understand how it works and future implementation - disabled submission forms*/
+// function setProfilePage(name : string, nickname : string, avatar : string)
+// {
+// 	profName.textContent = "Name: " + name;
+// 	profNickname.textContent = "NickName: " + nickname;
+// 	const imgElement = document.createElement("img");
+//     imgElement.src = avatar;
+//     profAvatar.appendChild(imgElement);
+// }
+
+// let usersList : FormData[] = [];
+// let newUser : FormData = new FormData();
+// newUser.append('name', 'sveva');
+// newUser.append('nickname', 'sve');
+// newUser.append('avatar', './images/blue.png');
+
+// let newUser1 : FormData = new FormData();
+// newUser.append('name', 'alena');
+// newUser.append('nickname', 'ale');
+// newUser.append('avatar', './images/green.png');
+
+// let newUser2 : FormData = new FormData();
+// newUser.append('name', 'luis');
+// newUser.append('nickname', 'lui');
+// newUser.append('avatar', './images/purple.png');
+
+
+// registerForm.addEventListener("submit", (event : any) => {
+
+// 	event.preventDefault()
+	
+// 	console.log('disable reg form submission');
+// 	alert('registration was a success');
+// 	setAccountPage('login');
+// });
+
+// loginForm.addEventListener("submit", (event : any) => {
+// 	event.preventDefault();
+
+// 	let loggedUser : FormData = new FormData();
+// 	loggedUser.append('name', 'sveva');
+// 	loggedUser.append('nickname', 'sve');
+// 	loggedUser.append('avatar', './images/blue.png');
+// 	console.log('disable log form submission');
+// 	userIsLoggedIn = true;
+// 	const nameEntry = loggedUser.get('name');
+// 	const nicknameEntry = loggedUser.get('nickname');
+// 	const avatarEntry = loggedUser.get('avatar');
+
+// 	const name = nameEntry !== null ? nameEntry.toString() : '';
+// 	const nickname = nicknameEntry !== null ? nicknameEntry.toString() : '';
+// 	const avatar = avatarEntry !== null ? avatarEntry.toString() : '';
+
+// 	setProfilePage(name, nickname, avatar);
+// 	console.log('page is visible?');
+// 	displayPage('account');
+// });
+
+
+
--- frontend/src/contentManager.ts
+++ frontend/src/contentManager.ts
@@ -1,5 +1,9 @@
 const chatP : any = document.getElementById('chatPage');
 const welcP : any = document.getElementById('welcomePage');
+const accP : any = document.getElementById('accountPage');
+const regP : any = document.getElementById('registerPage');
+const logP: any = document.getElementById('loginPage');
+const profP : any = document.getElementById('profilePage');
 
 function displayPage(text: string) : void
 {
@@ -7,11 +11,28 @@ function displayPage(text: string) : void
 	{
 		chatP.style.display = "grid";
 		welcP.style.display = "none";
+		accP.style.display = "none";
+		regP.style.display = "none";
+		logP.style.display = "none";
 	}
 	else if(text == 'welcome')
 	{
 		welcP.style.display = "flex";
 		chatP.style.display = "none";
+		accP.style.display = "none";
+		regP.style.display = "none";
+		logP.style.display = "none";
+	}
+	else if (text == 'account')
+	{
+		if (userIsLoggedIn == false)
+			accP.style.display = "flex";
+		else
+			profP.style.display = "flex";
+		chatP.style.display = "none";
+		welcP.style.display = "none";
+		regP.style.display = "none";
+		logP.style.display = "none";
 	}
 
 }
