7892eb99e2502ddb982585e49274ec0b96dacf89
Sveva Mazzari <smazzari88@gmail.com>
2025-11-19 22:55:57 +0100

    format all docs

--- frontend/src/contentManager.ts
+++ frontend/src/contentManager.ts
@@ -1,70 +1,60 @@
-const chatP : any = document.getElementById('chatPage');
-const welcP : any = document.getElementById('welcomePage');
-const accP : any = document.getElementById('accountPage');
-const gameP : any = document.getElementById('gamePage');
+const chatP: any = document.getElementById("chatPage");
+const welcP: any = document.getElementById("welcomePage");
+const accP: any = document.getElementById("accountPage");
+const gameP: any = document.getElementById("gamePage");
 
-
-
-function displayPage(text: string) : void
-{
-	if (text == 'chat')
-	{
-		chatP.classList.add("grid");
-		chatP.classList.remove("hidden");
-		welcP.classList.add("hidden");
-		welcP.classList.remove("flex");
-		accP.classList.add("hidden");
-		accP.classList.remove("flex");
-		gameP.classList.add("hidden");
-		gameP.classList.remove("flex");
-		setGame.classList.add("hidden");
-		setGame.classList.remove("block");
-		gameOverDiv.classList.add("hidden");
-		gameOverDiv.classList.remove("flex");
-		instruction.classList.add("hidden");
-  		instruction.classList.remove("flex");
-		if (gameisOn)
-		{
-			clearInterval(interval);
-			canvas.classList.add("hidden");
-			canvas.classList.remove("block");
-			gameisOn = false;
-		}
-	}
-	else if(text == 'welcome')
-	{
-		welcP.classList.add("flex");
-		welcP.classList.remove("hidden");
-		chatP.classList.remove("grid");
-		chatP.classList.add("hidden");
-		accP.classList.add("hidden");
-		accP.classList.remove("flex");
-		gameP.classList.add("hidden");
-		gameP.classList.remove("flex");
-		setGame.classList.add("hidden");
-		setGame.classList.remove("block");
-		gameOverDiv.classList.add("hidden");
-		gameOverDiv.classList.remove("flex");
-		instruction.classList.add("hidden");
-  		instruction.classList.remove("flex");
-		if (gameisOn)
-		{
-			clearInterval(interval);
-			canvas.classList.add("hidden");
-			canvas.classList.remove("block");
-			gameisOn = false;
-		}
-	}
-	else if (text == 'game' || text == 'tournament')
-	{
-		gameP.classList.add("flex");
-		gameP.classList.remove("hidden");
-		setGameType(text);
-		welcP.classList.add("hidden");
-		welcP.classList.remove("flex");
-		chatP.classList.remove("grid");
-		chatP.classList.add("hidden");
-		accP.classList.add("hidden");
-		accP.classList.remove("flex");
-	}
+function displayPage(text: string): void {
+  if (text == "chat") {
+    chatP.classList.add("grid");
+    chatP.classList.remove("hidden");
+    welcP.classList.add("hidden");
+    welcP.classList.remove("flex");
+    accP.classList.add("hidden");
+    accP.classList.remove("flex");
+    gameP.classList.add("hidden");
+    gameP.classList.remove("flex");
+    setGame.classList.add("hidden");
+    setGame.classList.remove("block");
+    gameOverDiv.classList.add("hidden");
+    gameOverDiv.classList.remove("flex");
+    instruction.classList.add("hidden");
+    instruction.classList.remove("flex");
+    if (gameisOn) {
+      clearInterval(interval);
+      canvas.classList.add("hidden");
+      canvas.classList.remove("block");
+      gameisOn = false;
+    }
+  } else if (text == "welcome") {
+    welcP.classList.add("flex");
+    welcP.classList.remove("hidden");
+    chatP.classList.remove("grid");
+    chatP.classList.add("hidden");
+    accP.classList.add("hidden");
+    accP.classList.remove("flex");
+    gameP.classList.add("hidden");
+    gameP.classList.remove("flex");
+    setGame.classList.add("hidden");
+    setGame.classList.remove("block");
+    gameOverDiv.classList.add("hidden");
+    gameOverDiv.classList.remove("flex");
+    instruction.classList.add("hidden");
+    instruction.classList.remove("flex");
+    if (gameisOn) {
+      clearInterval(interval);
+      canvas.classList.add("hidden");
+      canvas.classList.remove("block");
+      gameisOn = false;
+    }
+  } else if (text == "game" || text == "tournament") {
+    gameP.classList.add("flex");
+    gameP.classList.remove("hidden");
+    setGameType(text);
+    welcP.classList.add("hidden");
+    welcP.classList.remove("flex");
+    chatP.classList.remove("grid");
+    chatP.classList.add("hidden");
+    accP.classList.add("hidden");
+    accP.classList.remove("flex");
+  }
 }
--- frontend/src/setUpPlayers.ts
+++ frontend/src/setUpPlayers.ts
@@ -1,64 +1,60 @@
-const setGame : any = document.getElementById("setUpGame");
-const aliasPage : any = document.getElementById("setUpPlayers");
-const aliasPlayerThreeDiv : any = document.getElementById("aliasPlayerThree");
-const aliasPlayerFourDiv : any = document.getElementById("aliasPlayerFour");
-const aliasPlayerOneInput : any = document.getElementById("namePlayerOne");
-const aliasPlayerTwoInput : any = document.getElementById("namePlayerTwo");
-const aliasPlayerThreeInput : any = document.getElementById("namePlayerThree");
-const aliasPlayerFourInput : any = document.getElementById("namePlayerFour");
-const AIPlayerOneButtonsDiv : any = document.getElementById("AIPlayerOne");
-const AIPlayerTwoButtonsDiv : any = document.getElementById("AIPlayerTwo");
-const AIPlayerThreeButtonsDiv : any = document.getElementById("AIPlayerThree");
-const AIPlayerFourButtonsDiv : any = document.getElementById("AIPlayerFour");
-const aliasPlayerOneButton : any = document.getElementById("okPlayerOne");
-const aliasPlayerTwoButton : any = document.getElementById("okPlayerTwo");
-const aliasPlayerThreeButton : any = document.getElementById("okPlayerThree");
-const aliasPlayerFourButton : any = document.getElementById("okPlayerFour");
-const AIonButtonPlayerOne : any = document.getElementById("AIonPlayerOne");
-const AIonButtonPlayerTwo : any = document.getElementById("AIonPlayerTwo");
-const AIonButtonPlayerThree : any = document.getElementById("AIonPlayerThree");
-const AIonButtonPlayerFour : any = document.getElementById("AIonPlayerFour");
-const AIoffButtonPlayerOne : any = document.getElementById("AIoffPlayerOne");
-const AIoffButtonPlayerTwo : any = document.getElementById("AIoffPlayerTwo");
-const AIoffButtonPlayerThree : any = document.getElementById("AIoffPlayerThree");
-const AIoffButtonPlayerFour : any = document.getElementById("AIoffPlayerFour");
+const setGame: any = document.getElementById("setUpGame");
+const aliasPage: any = document.getElementById("setUpPlayers");
+const aliasPlayerThreeDiv: any = document.getElementById("aliasPlayerThree");
+const aliasPlayerFourDiv: any = document.getElementById("aliasPlayerFour");
+const aliasPlayerOneInput: any = document.getElementById("namePlayerOne");
+const aliasPlayerTwoInput: any = document.getElementById("namePlayerTwo");
+const aliasPlayerThreeInput: any = document.getElementById("namePlayerThree");
+const aliasPlayerFourInput: any = document.getElementById("namePlayerFour");
+const AIPlayerOneButtonsDiv: any = document.getElementById("AIPlayerOne");
+const AIPlayerTwoButtonsDiv: any = document.getElementById("AIPlayerTwo");
+const AIPlayerThreeButtonsDiv: any = document.getElementById("AIPlayerThree");
+const AIPlayerFourButtonsDiv: any = document.getElementById("AIPlayerFour");
+const aliasPlayerOneButton: any = document.getElementById("okPlayerOne");
+const aliasPlayerTwoButton: any = document.getElementById("okPlayerTwo");
+const aliasPlayerThreeButton: any = document.getElementById("okPlayerThree");
+const aliasPlayerFourButton: any = document.getElementById("okPlayerFour");
+const AIonButtonPlayerOne: any = document.getElementById("AIonPlayerOne");
+const AIonButtonPlayerTwo: any = document.getElementById("AIonPlayerTwo");
+const AIonButtonPlayerThree: any = document.getElementById("AIonPlayerThree");
+const AIonButtonPlayerFour: any = document.getElementById("AIonPlayerFour");
+const AIoffButtonPlayerOne: any = document.getElementById("AIoffPlayerOne");
+const AIoffButtonPlayerTwo: any = document.getElementById("AIoffPlayerTwo");
+const AIoffButtonPlayerThree: any = document.getElementById("AIoffPlayerThree");
+const AIoffButtonPlayerFour: any = document.getElementById("AIoffPlayerFour");
 
-let AIPlayerTwo : boolean = false;
-let AIPlayerThree : boolean = false;
-let AIPlayerFour : boolean = false;
-let isTournament : boolean = false;
+let AIPlayerTwo: boolean = false;
+let AIPlayerThree: boolean = false;
+let AIPlayerFour: boolean = false;
+let isTournament: boolean = false;
 
 let players: Player[] = [];
 
 class Player {
-    name: string;
-    isAI : boolean;
-    playerNum : number;
+  name: string;
+  isAI: boolean;
+  playerNum: number;
 
-    constructor(name : string, AI : boolean, num : number)
-    {
-      this.name = name;
-      this.isAI = AI;
-      this.playerNum = num;
-    }
+  constructor(name: string, AI: boolean, num: number) {
+    this.name = name;
+    this.isAI = AI;
+    this.playerNum = num;
+  }
 }
 
 //clean up page for choosing alias - making sure nothing left from previous session
-function setGameType(text : string)
-{
+function setGameType(text: string) {
   isTournament = false;
-  if (gameisOn)
-		{
-      console.log(`${gameisOn} - clean up screen`);
-			clearInterval(interval)
-      gameScreenDiv.classList.add("hidden");
-			gameScreenDiv.classList.remove("flex");
-			canvas.classList.add("hidden");
-			canvas.classList.remove("block");
-			gameisOn = false;
-		}
-  if (text == "tournament")
-    isTournament = true;
+  if (gameisOn) {
+    console.log(`${gameisOn} - clean up screen`);
+    clearInterval(interval);
+    gameScreenDiv.classList.add("hidden");
+    gameScreenDiv.classList.remove("flex");
+    canvas.classList.add("hidden");
+    canvas.classList.remove("block");
+    gameisOn = false;
+  }
+  if (text == "tournament") isTournament = true;
   setGame.classList.add("flex");
   setGame.classList.remove("hidden");
   players = [];
@@ -66,10 +62,10 @@ function setGameType(text : string)
   lastGameTournament = [];
   gameisOn = false;
   gameQueue = [];
-  aliasPlayerOneInput.value = '';
-  aliasPlayerTwoInput.value = '';
-  aliasPlayerThreeInput.value = '';
-  aliasPlayerFourInput.value = '';
+  aliasPlayerOneInput.value = "";
+  aliasPlayerTwoInput.value = "";
+  aliasPlayerThreeInput.value = "";
+  aliasPlayerFourInput.value = "";
 
   aliasPlayerOneInput.readOnly = false;
   aliasPlayerOneButton.disabled = false;
@@ -80,9 +76,21 @@ function setGameType(text : string)
   aliasPlayerFourInput.readOnly = false;
   aliasPlayerFourButton.disabled = false;
 
-  AIPlayerTwo = AIisOff(AIonButtonPlayerTwo, AIoffButtonPlayerTwo, aliasPlayerTwoInput);
-  AIPlayerThree = AIisOff(AIonButtonPlayerThree, AIoffButtonPlayerThree, aliasPlayerThreeInput);
-  AIPlayerFour = AIisOff(AIonButtonPlayerFour, AIoffButtonPlayerFour, aliasPlayerFourInput);
+  AIPlayerTwo = AIisOff(
+    AIonButtonPlayerTwo,
+    AIoffButtonPlayerTwo,
+    aliasPlayerTwoInput,
+  );
+  AIPlayerThree = AIisOff(
+    AIonButtonPlayerThree,
+    AIoffButtonPlayerThree,
+    aliasPlayerThreeInput,
+  );
+  AIPlayerFour = AIisOff(
+    AIonButtonPlayerFour,
+    AIoffButtonPlayerFour,
+    aliasPlayerFourInput,
+  );
 
   AIPlayerTwoButtonsDiv.classList.remove("invisible");
   AIPlayerThreeButtonsDiv.classList.remove("invisible");
@@ -98,17 +106,13 @@ function setGameType(text : string)
 }
 
 //set page for 2 players or 4 players (tournament)
-function aliasSelection()
-{
-  if (isTournament == false)
-  {
+function aliasSelection() {
+  if (isTournament == false) {
     aliasPlayerThreeDiv.classList.add("hidden");
     aliasPlayerThreeDiv.classList.remove("block");
     aliasPlayerFourDiv.classList.add("hidden");
     aliasPlayerFourDiv.classList.remove("block");
-  }
-  else
-  {
+  } else {
     aliasPlayerThreeDiv.classList.add("block");
     aliasPlayerThreeDiv.classList.remove("hidden");
     aliasPlayerFourDiv.classList.add("block");
@@ -117,18 +121,13 @@ function aliasSelection()
 }
 
 //check/add player to the list
-function checkAlias(name : string, AIFlag : boolean) : string
-{
-  console.log(`check alias name: ${name}`)
-  console.log(`check alias AI: ${AIFlag}`)
-  if (name == "AI" && !AIFlag)
-      return "";
-  else if (name == '')
-    return "";
-  else if (name == "AI")
-    return "Ai";
-  for (let i = 0; i < players.length; i++)
-  {
+function checkAlias(name: string, AIFlag: boolean): string {
+  console.log(`check alias name: ${name}`);
+  console.log(`check alias AI: ${AIFlag}`);
+  if (name == "AI" && !AIFlag) return "";
+  else if (name == "") return "";
+  else if (name == "AI") return "Ai";
+  for (let i = 0; i < players.length; i++) {
     if (players[i].name == name) {
       return "";
     }
@@ -136,40 +135,42 @@ function checkAlias(name : string, AIFlag : boolean) : string
   return name;
 }
 
-function addPlayer(name : string, AI : boolean, index : number)
-{
+function addPlayer(name: string, AI: boolean, index: number) {
   const player = new Player(name, AI, index);
   players.push(player);
 }
 
-function processInput(input : any, index : number, AIbuttonsDiv : any, AIFlag : boolean) : boolean
-{
+function processInput(
+  input: any,
+  index: number,
+  AIbuttonsDiv: any,
+  AIFlag: boolean,
+): boolean {
   console.log("process input");
   const name = checkAlias(input.value, AIFlag);
-  if (name)
-  {
-    console.log(`check alias AI before adding to player: ${AIFlag}`)
+  if (name) {
+    console.log(`check alias AI before adding to player: ${AIFlag}`);
     addPlayer(name, AIFlag, index);
-    console.log(`check alias AI after adding to player: ${AIFlag}`)
-    
+    console.log(`check alias AI after adding to player: ${AIFlag}`);
+
     input.readOnly = true;
     AIbuttonsDiv.classList.add("invisible");
     //add green text + check
     return true;
   }
-  input.value = '';
+  input.value = "";
   return false;
 }
 
 //Switch on/off AI button
-function AIisOn(onButton : any, offButton : any, input : any) : boolean{
+function AIisOn(onButton: any, offButton: any, input: any): boolean {
   onButton.classList.add("active");
   offButton.classList.remove("active");
   input.value = "AI";
   return true;
 }
 
-function AIisOff(onButton : any, offButton : any, input : any) : boolean{
+function AIisOff(onButton: any, offButton: any, input: any): boolean {
   onButton.classList.remove("active");
   offButton.classList.add("active");
   input.value = "";
@@ -178,56 +179,92 @@ function AIisOff(onButton : any, offButton : any, input : any) : boolean{
 
 //buttons on/off
 AIonButtonPlayerTwo.addEventListener("click", (even: any) => {
-  AIPlayerTwo = AIisOn(AIonButtonPlayerTwo, AIoffButtonPlayerTwo, aliasPlayerTwoInput);
+  AIPlayerTwo = AIisOn(
+    AIonButtonPlayerTwo,
+    AIoffButtonPlayerTwo,
+    aliasPlayerTwoInput,
+  );
 });
 
-AIoffButtonPlayerTwo.addEventListener("click", (event : any) => {
+AIoffButtonPlayerTwo.addEventListener("click", (event: any) => {
   console.log("is off AI");
-   AIPlayerTwo = AIisOff(AIonButtonPlayerTwo, AIoffButtonPlayerTwo, aliasPlayerTwoInput);
+  AIPlayerTwo = AIisOff(
+    AIonButtonPlayerTwo,
+    AIoffButtonPlayerTwo,
+    aliasPlayerTwoInput,
+  );
 });
 
 AIonButtonPlayerThree.addEventListener("click", (even: any) => {
-   AIPlayerThree = AIisOn(AIonButtonPlayerThree, AIoffButtonPlayerThree, aliasPlayerThreeInput);
+  AIPlayerThree = AIisOn(
+    AIonButtonPlayerThree,
+    AIoffButtonPlayerThree,
+    aliasPlayerThreeInput,
+  );
 });
 
-AIoffButtonPlayerThree.addEventListener("click", (event : any) => {
-  AIPlayerThree = AIisOff(AIonButtonPlayerThree, AIoffButtonPlayerThree, aliasPlayerThreeInput);
+AIoffButtonPlayerThree.addEventListener("click", (event: any) => {
+  AIPlayerThree = AIisOff(
+    AIonButtonPlayerThree,
+    AIoffButtonPlayerThree,
+    aliasPlayerThreeInput,
+  );
 });
 
 AIonButtonPlayerFour.addEventListener("click", (even: any) => {
-  AIPlayerFour = AIisOn(AIonButtonPlayerFour, AIoffButtonPlayerFour, aliasPlayerFourInput);
+  AIPlayerFour = AIisOn(
+    AIonButtonPlayerFour,
+    AIoffButtonPlayerFour,
+    aliasPlayerFourInput,
+  );
 });
 
-AIoffButtonPlayerFour.addEventListener("click", (event : any) => {
-  AIPlayerFour = AIisOff(AIonButtonPlayerFour, AIoffButtonPlayerFour, aliasPlayerFourInput);
+AIoffButtonPlayerFour.addEventListener("click", (event: any) => {
+  AIPlayerFour = AIisOff(
+    AIonButtonPlayerFour,
+    AIoffButtonPlayerFour,
+    aliasPlayerFourInput,
+  );
 });
 
 //buttons
 //process input and check if we can start the game
-aliasPlayerOneButton.addEventListener("click", () =>
-{
-  const startGame = processInput(aliasPlayerOneInput, 1, AIPlayerOneButtonsDiv, false);
-  if (startGame == true)
-      showPageBeforeGame();
+aliasPlayerOneButton.addEventListener("click", () => {
+  const startGame = processInput(
+    aliasPlayerOneInput,
+    1,
+    AIPlayerOneButtonsDiv,
+    false,
+  );
+  if (startGame == true) showPageBeforeGame();
 });
 
-aliasPlayerTwoButton.addEventListener("click", () =>
-{
-  const startGame = processInput(aliasPlayerTwoInput, 2, AIPlayerTwoButtonsDiv, AIPlayerTwo);
-  if (startGame == true)
-      showPageBeforeGame();
+aliasPlayerTwoButton.addEventListener("click", () => {
+  const startGame = processInput(
+    aliasPlayerTwoInput,
+    2,
+    AIPlayerTwoButtonsDiv,
+    AIPlayerTwo,
+  );
+  if (startGame == true) showPageBeforeGame();
 });
 
-aliasPlayerThreeButton.addEventListener("click", () =>
-{
-  const startGame = processInput(aliasPlayerThreeInput, 3, AIPlayerThreeButtonsDiv, AIPlayerThree);
-  if (startGame == true)
-      showPageBeforeGame();
+aliasPlayerThreeButton.addEventListener("click", () => {
+  const startGame = processInput(
+    aliasPlayerThreeInput,
+    3,
+    AIPlayerThreeButtonsDiv,
+    AIPlayerThree,
+  );
+  if (startGame == true) showPageBeforeGame();
 });
 
-aliasPlayerFourButton.addEventListener("click", () =>
-{
-  const startGame = processInput(aliasPlayerFourInput, 4, AIPlayerFourButtonsDiv, AIPlayerFour);
-  if (startGame == true)
-      showPageBeforeGame();
+aliasPlayerFourButton.addEventListener("click", () => {
+  const startGame = processInput(
+    aliasPlayerFourInput,
+    4,
+    AIPlayerFourButtonsDiv,
+    AIPlayerFour,
+  );
+  if (startGame == true) showPageBeforeGame();
 });
