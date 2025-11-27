699a38889623e1455468d08e91442a8dccb2f203
Alena <livionak@gmail.com>
2025-11-27 21:48:19 +0100

    added types for api

--- backend/src/lib/types/api.ts
+++ backend/src/lib/types/api.ts
@@ -43,10 +43,10 @@ export type LoginBody = {
 };
 
 
-  export type ChangePasswordBody = {
-    currentPassword: PasswordPlain;
-    newPassword: PasswordPlain;
-  };
+export type ChangePasswordBody = {
+	currentPassword: PasswordPlain;
+	newPassword: PasswordPlain;
+};
 
 //_________PROFILE__send_ to_FRONTEND___________________
 
@@ -91,26 +91,42 @@ export type ReceiverId = PrivateSenderId;
 export type MessageContent = string;
 
 export type ChatMessageType =
-  | "PrivateMessage"
-  | "PrivateGameInviteMessage"
-  | "TournamentMessage";
-  
+	| "PrivateMessage"
+	| "PrivateGameInviteMessage"
+	| "TournamentMessage";
+
+
+export type ChatMessage = {
+	type: ChatMessageType;
+	senderId: number | string; // user id OR SystemId
+	content: string; // what to display
+};
+
+
 // Body that client sends for send message
 
 type SendGameInviteBody = {
-  receiverId: Domain.ReceiverId;
-  // no content, backend will use MESSAGE_GAME_INVITE
+	receiverId: Domain.ReceiverId;
+	// no content, backend will use MESSAGE_GAME_INVITE
 };
 
 
 type SendPrivateMessageBody = {
-  receiverId: ReceiverId;
-  content: MessageContent;
-  // no type, no senderId
+	receiverId: ReceiverId;
+	content: MessageContent;
+	// no type, no senderId
 };
 
 type SendTournamentMessageBody = {
-  receiverId: ReceiverId;
-  meta:
+	receiverId: ReceiverId;
   // no content, backend will use MESSAGE_GAME_INVITE
+};
+
+
+export type GetChatParams = {
+  userId: number;    // or chat partner id
+};
+
+export type GetChatResponse = {
+  messages: ChatMessage[];
 };
