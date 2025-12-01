3445e4919d8199a209b9f7b9e3e3bdfbaeb673e7
Sveva Mazzari <smazzari88@gmail.com>
2025-12-01 13:20:28 +0100

    add fetch for login

--- frontend/src/loginUser.ts
+++ frontend/src/loginUser.ts
@@ -1,51 +1,75 @@
+const log : any = document.getElementById("login");
+const errorUsernameLog: any = document.getElementById("log-username_error");
+const errorPasswordLog: any = document.getElementById("log-password_error");
+const logUserHeaderDiv : any = document.getElementById("logged-user");
+const logAvatHeaderDiv : any = document.querySelector('#logged-user-avatar img');
+let loggedUser : boolean = false;
 
-const logg : any = document.getElementById('login');
+if (localStorage.getItem('loggedUser'))
+{
+    logUserHeaderDiv.textContent = `Hello, ${localStorage.getItem('loggedUser')}`;
+    logAvatHeaderDiv.src = localStorage.getItem('loggedAvatar');
+}
 
-const errorMess : any = document.getElementById('log-errors');
-errorMess.classList.add("text-red");
- const successMess : any = document.getElementById('log-success');
-        successMess.textContent = 'Registration successful!';
+log.addEventListener("submit", async (event: any) => {
+  event.preventDefault();
+    errorUsernameLog.classList.add("hidden");
+        errorUsernameLog.classList.remove("block");
+        errorPasswordLog.classList.remove("block");
+        errorPasswordLog.classList.add("hidden");
+  console.log("button login pressed");
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  const formData = new FormData(log);
 
-logg.addEventListener('submit', async (event : any) => {
-    event.preventDefault();
-
-    console.log("button login pressed");
-    const formData = new FormData(event.target);
-
-    const LoginBody = {
-        username: formData.get('login-user_username'),
-        passwordPlain: formData.get('login-user_password'),
-    };
-
-    try {
-        const response = await fetch('http://127.0.0.1:3000/auth/login', {
-            method: 'POST',
-            body: JSON.stringify(LoginBody),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-
-        if (!response.ok) {
-            const error = await response.json();
-            errorMess.textContent = `Error ${error.code}: ${error.error}`;
-            errorMess.classList.add("block");
-            errorMess.classList.remove("hidden");
-            successMess.classList.add("hidden");
-            successMess.classList.remove("block");
-            throw new Error(`Error ${error.code}: ${error.error}`);
-        }
-
-        const data = await response.json();
-         errorMess.textContent = '';
-
-        successMess.classList.add("block");
-        successMess.classList.remove("hidden");
-        successMess.classList.add("text-green");
-        errorMess.classList.add("hidden");
-        errorMess.classList.remove("block");
-        console.log('Login user:', data);
-    } catch (error) {
-        console.error('Error during registration:', error);
+  const LoginBody = {
+    username: formData.get("login-user_username"), // Use the name attribute
+    passwordPlain: formData.get("login-user_password"), // Use the name attribute
+  };
+  const myRequest = new Request("http://127.0.0.1:3000/auth/login", {
+    method: "POST",
+    body: JSON.stringify(LoginBody),
+    headers: myHeaders,
+  });
+  try {
+    const response = await fetch(myRequest);
+    console.log(response);
+    const data = await response.json();
+    if (!response.ok) {
+      if (data.field == "username") {
+        errorUsernameLog.classList.add("block");
+        errorUsernameLog.classList.remove("hidden");
+        errorUsernameLog.textContent = data.error;
+      } else {
+        errorPasswordLog.classList.add("block");
+        errorPasswordLog.classList.remove("hidden");
+        errorPasswordLog.textContent = data.error;
+      }
+      throw new Error(`Error ${response.status}`);
     }
+    else
+    {
+        loggedUser = true;
+        errorUsernameLog.classList.add("hidden");
+        errorUsernameLog.classList.remove("block");
+        errorPasswordLog.classList.remove("block");
+        errorPasswordLog.classList.add("hidden");
+        log.reset();
+        logUserHeaderDiv.textContent = '';
+        logUserHeaderDiv.textContent = `Hello, ${data.username}`;
+        const user = data.username;
+        let url : string = data.avatarUrl;
+        //need to change menu bar, change the name in the header and the avatar
+        if (url == null)
+            localStorage.setItem('loggedAvatar', "images/profile/blue.png");
+        else
+            localStorage.setItem('loggedAvatar', url);
+        localStorage.setItem('loggedUser', user);
+        logUserHeaderDiv.textContent = `Hello, ${localStorage.getItem('loggedUser')}`;
+        logAvatHeaderDiv.src = localStorage.getItem('loggedAvatar');
+        console.log("login user:", data);
+    }
+  } catch (error) {
+    console.error("Error during registration:", error);
+  }
 });
--- frontend/src/registerUser.ts
+++ frontend/src/registerUser.ts
@@ -1,30 +1,23 @@
-const reg : any = document.getElementById("register");
-const usernameInput: any = document.getElementById("reg-username");
-const usernameInputDiv : any = document.getElementById("usernameInput");
-const errorUsername : any = document.getElementById("reg-usernama_error");
+const reg: any = document.getElementById("register");
+const errorUsername: any = document.getElementById("reg-usernama_error");
 const errorDisplayName: any = document.getElementById("reg-displayName_error");
-const errorPaassword : any = document.getElementById("reg-password_error");
-const displayInput: any = document.getElementById("reg-display");
-const passwordInput: any = document.getElementById("reg-password");
+const errorPaassword: any = document.getElementById("reg-password_error");
 const successMessageDiv: any = document.getElementById("success-display");
-const loginButtonRedirect : any = document.getElementById("login-button");
+const loginButtonRedirect: any = document.getElementById("login-button");
 
 loginButtonRedirect.addEventListener("click", () => {
   successMessageDiv.classList.add("hidden");
   successMessageDiv.classList.remove("flex");
-  setAccountPage('login');
-})
+  setAccountPage("login");
+});
 reg.addEventListener("submit", async (event: any) => {
   event.preventDefault();
-errorUsername.classList.add("hidden");
-errorUsername.classList.remove("block");
-errorDisplayName.classList.add("hidden");
-errorDisplayName.classList.remove("block");
-errorPaassword.classList.add("hidden");
-errorPaassword.classList.remove("block");
-successMessageDiv.classList.add("hidden");
-successMessageDiv.classList.remove("flex");
-  console.log("button pressed");
+  errorUsername.classList.add("hidden");
+  errorUsername.classList.remove("block");
+  errorDisplayName.classList.add("hidden");
+  errorDisplayName.classList.remove("block");
+  errorPaassword.classList.add("hidden");
+  errorPaassword.classList.remove("block");
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
   const formData = new FormData(event.target);
@@ -46,37 +39,32 @@ successMessageDiv.classList.remove("flex");
     console.log(response.json);
     const data = await response.json();
     if (!response.ok) {
-        if (data.field == "username")
-        {
-            errorUsername.classList.add("block");
-            errorUsername.classList.remove("hidden");
-            errorUsername.textContent = data.error;
-        }
-        else if (data.field == "displayName")
-        {
-            errorDisplayName.classList.add("block");
-            errorDisplayName.classList.remove("hidden");
-            errorDisplayName.textContent = data.error;
-        }
-        else
-        {
-            errorPaassword.classList.add("block");
-            errorPaassword.classList.remove("hidden");
-            errorPaassword.textContent = data.error;
-        }
+      if (data.field == "username") {
+        errorUsername.classList.add("block");
+        errorUsername.classList.remove("hidden");
+        errorUsername.textContent = data.error;
+      } else if (data.field == "displayName") {
+        errorDisplayName.classList.add("block");
+        errorDisplayName.classList.remove("hidden");
+        errorDisplayName.textContent = data.error;
+      } else {
+        errorPaassword.classList.add("block");
+        errorPaassword.classList.remove("hidden");
+        errorPaassword.textContent = data.error;
+      }
       throw new Error(`Response status ${response.status}`);
     } else {
-        errorUsername.classList.add("hidden");
-        errorUsername.classList.remove("block");
-        errorDisplayName.classList.add("hidden");
-        errorDisplayName.classList.remove("block");
-        errorPaassword.classList.add("hidden");
-        errorPaassword.classList.remove("block");
+      errorUsername.classList.add("hidden");
+      errorUsername.classList.remove("block");
+      errorDisplayName.classList.add("hidden");
+      errorDisplayName.classList.remove("block");
+      errorPaassword.classList.add("hidden");
+      errorPaassword.classList.remove("block");
       successMessageDiv.classList.add("flex");
       successMessageDiv.classList.remove("hidden");
       reg.classList.add("hidden");
       reg.classList.remove("flex");
-       reg.reset();
+      reg.reset();
       console.log("Registered user:", data);
     }
   } catch (error: any) {
