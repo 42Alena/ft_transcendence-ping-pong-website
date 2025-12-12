e5f6e2267bd3e4adf4de7cb77a7eed5b19e12a9c
Alena <livionak@gmail.com>
2025-12-12 16:31:23 +0100

    added domain aliases for check players in match and tournament

--- backend/src/lib/types/domain.ts
+++ backend/src/lib/types/domain.ts
@@ -445,3 +445,29 @@ export type UserProfileStats = {
 	place3: number;           // how many times 3rd place
 };
 
+//__________________GAME: ALIAS CHECK (MIN)______________
+
+export type CheckMatchAliasesResult =
+	| {
+		ok: true;
+		player1Alias: Alias;
+		player2Alias: Alias;
+	}
+	| {
+		ok: false;
+		error: string;
+	};
+
+	
+export type CheckTournamentAliasesResult =
+	| {
+		ok: true;
+		player1Alias: Alias;
+		player2Alias: Alias;
+		player3Alias: Alias;
+		player4Alias: Alias;
+	}
+	| {
+		ok: false;
+		error: string;
+	};
