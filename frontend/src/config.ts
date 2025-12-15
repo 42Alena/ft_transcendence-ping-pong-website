1ed21c012a5f9615d016a5ac0abeb8bcd818f621
Alena Kurmyza <livionak@gmail.com>
2025-12-15 11:53:07 +0100

    Revert "Alena/spa history (#183)"
    This reverts commit b0671097353ad10308812e6a22854d21ddd46783.

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,9 +1,11 @@
+// detect if we are inside https docker env or just local
+const base_url = (window.location.protocol === 'https:') ?
+	window.location.href :   // https://localhost:8443/ without hash
+	`http://${window.location.hostname}:3000`;
 // same hostname is important for cookies! if frontend is localhost:8080 then backend needs to be localhost:3000
 // if frontend is 127.0.0.1:8080 then backend will use 127.0.0.1:3000 for cookies to work
 
-var backendPort =
-  window.location.protocol === "https:" ? window.location.port : "3000";
-var BACKEND_URL = `${window.location.protocol}//${window.location.hostname}:${backendPort}`;
+var BACKEND_URL = (base_url.split('#')[0]).replace(/\/$/, ''); // remove trailing / and '#'
 
 // (window as any).BACKEND_URL = BACKEND_URL;
-console.log("Detected BACKEND url", BACKEND_URL);
+console.log('Detected BACKEND url', BACKEND_URL);
