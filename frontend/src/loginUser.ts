74ae8040deb8cceb267693e34cb99c19b491d9c3
svevotti <111067594+svevotti@users.noreply.github.com>
2025-12-15 19:41:59 +0100

    Sveva/history api (#184)
    added history api : back and forth works, but not yet for
    chat->users->profile. it works for chat
    added responsive layout: missing only the game board - I will not allow
    if the screen is too small
    able to see at own profile even after checking user' profile from chat
    
    ---------
    
    Co-authored-by: 42Alena <livionak@gmail.com>

--- frontend/src/loginUser.ts
+++ frontend/src/loginUser.ts
@@ -1,4 +1,4 @@
-const log: any = document.getElementById("login");
+const log: any = document.getElementById("loginForm");
 const errorUsernameLog: any = document.getElementById("log-username_error");
 const errorPasswordLog: any = document.getElementById("log-password_error");
 const logUserHeaderDiv: any = document.getElementById("logged-user");
@@ -41,7 +41,6 @@ log.addEventListener("submit", async (event: any) => {
   errorUsernameLog.classList.remove("block");
   errorPasswordLog.classList.remove("block");
   errorPasswordLog.classList.add("hidden");
-  console.log("button login pressed");
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
   const formData = new FormData(log);
@@ -50,7 +49,6 @@ log.addEventListener("submit", async (event: any) => {
     username: formData.get("login-user_username"),
     passwordPlain: formData.get("login-user_password"),
   };
-  // const myRequest = new Request("http://127.0.0.1:3000/auth/login", {
   const myRequest = new Request(`${BACKEND_URL}/auth/login`, {
     method: "POST",
     body: JSON.stringify(LoginBody),
@@ -59,7 +57,6 @@ log.addEventListener("submit", async (event: any) => {
   });
   try {
     const response = await fetch(myRequest);
-    console.log(response);
     const data = await response.json();
     if (!response.ok) {
       if (data.field == "username") {
@@ -83,22 +80,22 @@ log.addEventListener("submit", async (event: any) => {
       const userDataString: string | null = localStorage.getItem("userData");
       if (userDataString) {
         const userData = JSON.parse(userDataString);
-        userData.avatarUrl = "/images/profile/blue.png"; //default img
+        userData.avatarUrl =
+          data.avatarUrl || "images/avatars/pong_default.png";
         logUserHeaderDiv.textContent = `Hello, ${userData.username}`;
         logAvatHeaderDiv.src = userData.avatarUrl;
         dropMenuUserDiv.classList.add("block");
         dropMenuUserDiv.classList.remove("hidden");
         dropMenuGuestDiv.classList.add("hidden");
         dropMenuGuestDiv.classList.remove("block");
-        displayPage("welcome");
+        handleClickEvent("welcome");
       }
       console.log("login user:", data);
     }
   } catch (error) {
-    console.error("Error during registration:", error);
+    console.error("Error during login:", error);
   }
 });
 
 //need to change fastify.register(require('@fastify/cors'), { origin: '*' })
 //fastify.register(require('@fastify/cors'), { origin: ['http://127.0.0.1:8081', 'http://localhost:8081'], credentials: true}) //https://github.com/fastify/fastify-cors
-
--- frontend/src/logoutUser.ts
+++ frontend/src/logoutUser.ts
@@ -1,19 +1,10 @@
 const logoutButton: any = document.getElementById("logout") as HTMLButtonElement;
 
 logoutButton.addEventListener("click", async (event: any) => {
-
-  localStorage.removeItem("userData");
-  dropMenuUserDiv.classList.add("hidden");
-  dropMenuUserDiv.classList.remove("block");
-  dropMenuGuestDiv.classList.add("block");
-  dropMenuGuestDiv.classList.remove("hidden");
-  logUserHeaderDiv.textContent = "Hello, you";
-  logAvatHeaderDiv.src = "images/profile/orange.png"; 
-  displayPage('welcome');
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
 
-  const myRequest = new Request("http://127.0.0.1:3000/auth/logout", {
+  const myRequest = new Request(`${BACKEND_URL}/auth/logout`, {
     method: "POST",
     headers: myHeaders,
     body: JSON.stringify({}),
@@ -27,10 +18,14 @@ logoutButton.addEventListener("click", async (event: any) => {
       throw new Error(`Response status ${response.status}`);
     } else {
       localStorage.clear();
+      console.log("logout");
       dropMenuUserDiv.classList.add("hidden");
       dropMenuUserDiv.classList.remove("block");
       dropMenuGuestDiv.classList.add("block");
       dropMenuGuestDiv.classList.remove("hidden");
+      logUserHeaderDiv.textContent = "Hello, you";
+      logAvatHeaderDiv.src = "images/profile/orange.png"; 
+      handleClickEvent('welcome');
       // document.cookie = "auth=; path=/auth; expires=Thu, 01 Jan 1970 00:00:00 GMT;"; // delete empty cookie
     }
   } catch (error: any) {
