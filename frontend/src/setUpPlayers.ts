cf6785dbde3f15ed64e6ca3b3058412ad1d6d93a
Sveva Mazzari <smazzari88@gmail.com>
2025-12-16 09:39:50 +0100

    fix to "skip" first object when getting back the players list

--- frontend/src/setUpPlayers.ts
+++ frontend/src/setUpPlayers.ts
@@ -1,13 +1,13 @@
 const setGame: any = document.getElementById("setUpGame");
 const aliasPage: any = document.getElementById("setUpPlayers");
 
-let AIPlayerOne : boolean = false;
+let AIPlayerOne: boolean = false;
 let AIPlayerTwo: boolean = false;
 let AIPlayerThree: boolean = false;
 let AIPlayerFour: boolean = false;
 let isTournament: boolean = false;
 
-const imgElement : any = document.querySelector('#gamePage img');
+const imgElement: any = document.querySelector("#gamePage img");
 
 let players: Player[] = [];
 
@@ -42,7 +42,7 @@ function setGameType(text: string) {
   lastGameTournament = [];
   gameisOn = false;
   gameQueue = [];
-  
+
   aliasPage.classList.add("flex");
   aliasPage.classList.remove("hidden");
   aliasSelection();
@@ -54,27 +54,33 @@ function setGameType(text: string) {
 
 //set page for 2 players or 4 players (tournament)
 function aliasSelection() {
-  const playerInputOne = document.getElementById("player-one") as HTMLInputElement;
-  const playerInputThree = document.getElementById("playerThree") as HTMLDivElement;
-  const playerInputFour = document.getElementById("playerFour") as HTMLDivElement;
+  const playerInputOne = document.getElementById(
+    "player-one",
+  ) as HTMLInputElement;
+  const playerInputThree = document.getElementById(
+    "playerThree",
+  ) as HTMLDivElement;
+  const playerInputFour = document.getElementById(
+    "playerFour",
+  ) as HTMLDivElement;
   playerInputOne.readOnly = false;
   if (localStorage.getItem("userData")) {
-  const userDataString: string | null = localStorage.getItem("userData");
-  if (userDataString) {
-    const userData = JSON.parse(userDataString);
-    console.log(userData.displayName);
-    playerInputOne.value = userData.displayName;
-    playerInputOne.readOnly = true;
+    const userDataString: string | null = localStorage.getItem("userData");
+    if (userDataString) {
+      const userData = JSON.parse(userDataString);
+      console.log(userData.displayName);
+      playerInputOne.value = userData.displayName;
+      playerInputOne.readOnly = true;
+    }
   }
-}
   if (isTournament == false) {
-    imgElement.src = "images/pages_images/pong_game_new.png"
+    imgElement.src = "images/pages_images/pong_game_new.png";
     playerInputThree.classList.remove("block");
     playerInputFour.classList.remove("block");
     playerInputThree.classList.add("hidden");
     playerInputFour.classList.add("hidden");
   } else {
-    imgElement.src = "images/pages_images/pong_tournament_new.png"
+    imgElement.src = "images/pages_images/pong_tournament_new.png";
     playerInputThree.classList.remove("hidden");
     playerInputFour.classList.remove("hidden");
     playerInputThree.classList.add("block");
@@ -87,10 +93,14 @@ function addPlayer(name: string, AI: boolean, index: number) {
   players.push(player);
 }
 
-const playersNameForm = document.getElementById("playersName") as HTMLFormElement;
-const errorNamesDiv = document.getElementById("playersName_error") as HTMLDivElement;
+const playersNameForm = document.getElementById(
+  "playersName",
+) as HTMLFormElement;
+const errorNamesDiv = document.getElementById(
+  "playersName_error",
+) as HTMLDivElement;
 
-playersNameForm.addEventListener("submit", async (event : SubmitEvent) => {
+playersNameForm.addEventListener("submit", async (event: SubmitEvent) => {
   event.preventDefault();
   errorNamesDiv.classList.add("invisible");
   const myHeaders = new Headers();
@@ -98,20 +108,18 @@ playersNameForm.addEventListener("submit", async (event : SubmitEvent) => {
   const formData = new FormData(playersNameForm);
   const playersNames = {
     player1Alias: formData.get("player-one_name") as string,
-    player2Alias: formData.get("player-two_name") as string, 
-    player3Alias: formData.get("player-three_name") as string, 
+    player2Alias: formData.get("player-two_name") as string,
+    player3Alias: formData.get("player-three_name") as string,
     player4Alias: formData.get("player-four_name") as string,
   };
   let url;
-  if (!isTournament)
-    url = `${BACKEND_URL}/games/match/aliases/check`;
-  else
-    url = `${BACKEND_URL}/games/tournament/aliases/check`;
+  if (!isTournament) url = `${BACKEND_URL}/games/match/aliases/check`;
+  else url = `${BACKEND_URL}/games/tournament/aliases/check`;
   const myRequest = new Request(url, {
     method: "POST",
     body: JSON.stringify(playersNames),
     headers: myHeaders,
-    credentials : "include"
+    credentials: "include",
   });
   try {
     const response = await fetch(myRequest);
@@ -119,23 +127,25 @@ playersNameForm.addEventListener("submit", async (event : SubmitEvent) => {
     console.log(response.json);
     const data = await response.json();
     if (!response.ok) {
-        errorNamesDiv.classList.remove("invisible");
-        errorNamesDiv.textContent = data.error;
+      errorNamesDiv.classList.remove("invisible");
+      errorNamesDiv.textContent = data.error;
       throw new Error(`Response status ${response.status}`);
     } else {
       errorNamesDiv.classList.add("invisible");
       let i = 0;
       for (const key in data) {
-            let AI : boolean = false;
-            const value = data[key];
-            if (value == "AI_ALENA" || value == "AI_SVEVA" || value == "AI_LUIS")
-            {
-              AI = true;
-            }
-            addPlayer(value, AI, i);
-            i++;
-        }
-      playersNameForm.reset()
+        let AI: boolean = false;
+        if (key != "ok")
+        {
+          const value = data[key];
+          if (value == "AI_ALENA" || value == "AI_SVEVA" || value == "AI_LUIS") {
+            AI = true;
+          }
+          addPlayer(value, AI, i);
+      }
+        i++;
+      }
+      playersNameForm.reset();
       showPageBeforeGame();
       console.log("check alias user:", data);
     }
@@ -143,4 +153,3 @@ playersNameForm.addEventListener("submit", async (event : SubmitEvent) => {
     console.error("Error during check alias:", error.message);
   }
 });
-
