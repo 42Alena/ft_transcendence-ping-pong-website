07eee87c1a9801eaabf0d246b3fac9465b28865c
42Alena <livionak@gmail.com>
2025-12-16 12:50:21 +0100

    remove extra data on createPieChart

--- frontend/src/profileUser.ts
+++ frontend/src/profileUser.ts
@@ -644,9 +644,9 @@ async function requestStats(id: string) {
       }
       // createPieChart(data.stats.data.winRatePercent, data.stats.lossRatePercent);
       // createBarChart(data.stats.place1, data.stats.place2, data.stats.place3);
-      
+
       // Alena:since you attached them to window, call via window
-      window.createPieChart(data.stats.data.winRatePercent, data.stats.lossRatePercent);
+      window.createPieChart(data.stats.winRatePercent, data.stats.lossRatePercent);
       window.createBarChart(data.stats.place1, data.stats.place2, data.stats.place3);
     }
   } catch (error) {
